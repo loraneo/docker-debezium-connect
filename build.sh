3b2aeb3207b1249464c2ce6d4d4f2d910ad99259
andrejas <andrejas@andrejas-laptop>
2018-08-16 20:11:50 +0200

    sa

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-mkdir target 
-cd target 
-git clone https://github.com/confluentinc/kafka-connect-elasticsearch.git
-cd kafka-connect-elasticsearch
-git checkout v5.0.0
-mvn package
 
 docker build -t loraneo/docker-kafka-connect:1.0.0a .
